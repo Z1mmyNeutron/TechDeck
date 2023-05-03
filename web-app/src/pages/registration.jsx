15ad4471cf969acbe658079fb030a648b34031f0
Chrissie Zimmer <czimmer12@aol.com>
2023-05-03 12:52:46 -0400

    s

---
+++ web-app/src/pages/registration.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import { postData } from '../serverApi/server';
+
+export function Registration() {
+  
+    return (
+        <div class="topLevel">
+          
+         <p>Congradulations! You have made a new account.</p>
+        </div>
+    )
+}
